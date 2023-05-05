d26f98dc749d82452ef04895a0378188907e2852
personal <juliodc289@gmail.com>
2023-05-05 10:59:35 -0400

    added scrollview

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import {
+	StyleSheet,
+	Text,
+	View,
+	Button,
+	TextInput,
+	ScrollView,
+} from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [enteredGoal, setEnteredGoal] = useState('')
+	const [goals, setGoals] = useState([])
+
+	function goalInputHandler(enteredText) {
+		setEnteredGoal(enteredText)
+	}
+
+	function addGoalHandler() {
+		setGoals((currentGoals) => [...currentGoals, enteredGoal])
+	}
+
+	return (
+		<View style={styles.appContainer}>
+			<View style={styles.inputContainer}>
+				<TextInput
+					style={styles.textInput}
+					placeholder='Your goals'
+					onChangeText={goalInputHandler}
+				/>
+				<Button title='ADD GOAL' onPress={addGoalHandler} />
+			</View>
+
+			<View style={styles.goalsContainer}>
+				<ScrollView>
+					<Text>List of goals</Text>
+
+					{goals.map((goal, index) => (
+						<View style={styles.goalItem} key={index}>
+							<Text style={styles.goalText}>{goal}</Text>
+						</View>
+					))}
+				</ScrollView>
+			</View>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	appContainer: {
+		flex: 1,
+		paddingTop: 55,
+		paddingHorizontal: 16,
+	},
+
+	inputContainer: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		marginBottom: 24,
+		borderBottomWidth: 1,
+		borderBottomColor: '#cccccc',
+	},
+
+	textInput: {
+		borderWidth: 1,
+		borderColor: '#cccccc',
+		width: '70%',
+		marginRight: 8,
+		padding: 8,
+	},
+
+	goalsContainer: {
+		flex: 5,
+	},
+
+	goalItem: {
+		padding: 8,
+		borderRadius: 6,
+		borderWidth: 1,
+		backgroundColor: '#5e0acc',
+		margin: 8,
+	},
+
+	goalText: {
+		color: 'white',
+	},
+})
